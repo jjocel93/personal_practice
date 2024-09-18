4f4528a13654f1111be823f7ea6b5ebf6323311c
jjocel93 <jjocel93@gmail.com>
2024-09-19 00:56:21 +0200

    change fill of SVGs. add js file. add slider photos to repo. create slider in html and js to make it work.

---
+++ lp_9_14_24_grid/script.js
@@ -0,0 +1,37 @@
+// Image slider
+
+const slides = document.querySelectorAll('.slides img');
+let slideIndex = 0;
+let intervalID = null;
+
+document.addEventListener('DOMContentLoaded', initializeSlider);
+
+function initializeSlider() {
+  if (slides.length > 0) {
+    slides[slideIndex].classList.add('displaySlide');
+    intervalID = setInterval(nextSlide, 7000);
+  }
+}
+
+function showSlide(index) {
+  if (index >= slides.length) {
+    slideIndex = 0;
+  } else if (index < 0) {
+    slideIndex = slides.length - 1;
+  }
+
+  slides.forEach((slide) => {
+    slide.classList.remove('displaySlide');
+  });
+  slides[slideIndex].classList.add('displaySlide');
+}
+
+function prevSlide() {
+  slideIndex--;
+  showSlide(slideIndex);
+}
+
+function nextSlide() {
+  slideIndex++;
+  showSlide(slideIndex);
+}
